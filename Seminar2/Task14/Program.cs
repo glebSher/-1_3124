d246eb6011f9f621165ebb573e670fb6fbb42808
glebshcherbakov <gleb.sher309@gmail.com>
2022-09-02 22:23:06 +0300

    Решение задачи 14

--- Seminar2/Task14/Program.cs
+++ Seminar2/Task14/Program.cs
@@ -4,14 +4,15 @@
 // 46 -> нет
 // 161 -> да
 
-Console.WriteLine ("Введите число");
+object value = Console.WriteLine ("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if number % 7 == 0 & number % 23 == 0;
+if (number % 7 == 0 & number % 23 == 0);
 {
-    Console.WriteLine("Да");
+    Console.WriteLine ("Да");
 }
+
 else
 {
-    Console.WriteLine("Нет");
+    Console.WriteLine ("Нет");
 }
