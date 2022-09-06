6712e3cc0131c8f56d62b983a35db96080a68248
glebshcherbakov <gleb.sher309@gmail.com>
2022-09-06 14:27:29 +0300

    Корректировка решения задачи 03

--- Seminar1/Task03/Program.cs
+++ Seminar1/Task03/Program.cs
@@ -10,31 +10,31 @@ if(number == 1)
 {
     Console.WriteLine("Понедельник");
 }
-if(number == 2)
+else if(number == 2)
 {
     Console.WriteLine("Вторник");
 }
-if(number == 3)
+else if(number == 3)
 {
     Console.WriteLine("Среда");
 }
-if(number == 4)
+else if(number == 4)
 {
     Console.WriteLine("Четверг");
 }
-if(number == 5)
+else if(number == 5)
 {
     Console.WriteLine("Пятница");
 }
-if (number == 6)
+else if (number == 6)
 {
     Console.WriteLine("Суббота");
 }
-if (number == 7)
+else if (number == 7)
 {
     Console.WriteLine("Воскресенье");
 }
-if (number > 7)
+else
 {
-    Console.WriteLine("В неделе 7 дней");
+    Console.WriteLine("Некорректный номер");
 }
