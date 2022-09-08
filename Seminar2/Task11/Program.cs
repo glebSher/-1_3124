44b682134274c19d9fcbf2811c1589b36fd214bb
glebshcherbakov <gleb.sher309@gmail.com>
2022-09-08 14:26:14 +0300

    Correction task numbering

--- Seminar2/Task11/Program.cs
+++ Seminar2/Task11/Program.cs
@@ -1,22 +1,16 @@
-﻿// Задача 12: Напишите программу, которая будет принимать на вход два числа и выводить, 
-// является ли первое число кратным второму. Если число 1 не кратно числу 2, 
-// то программа выводит остаток от деления.
-// 34, 5 -> не кратно, остаток 4
-// 16, 4 -> кратно
+﻿// Задача 11: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую
+// цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
 
-int number1, number2;
-Console.WriteLine("Введите два числа и программа проверит, является первое число кратным второму");
-number1 = Convert.ToInt32 (Console.ReadLine());
-number2 = Convert.ToInt32 (Console.ReadLine());
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.WriteLine($"Сгенерировалось случайное число {number}");
 
-int mod = number1 % number2;
+int digit1 = number / 100;
+int digit2 = number % 10;
 
-if (mod == 0)
-{
-    Console.WriteLine ($"Число {number1} кратно числу {number2}");
-}
-else 
-{
-    Console.WriteLine ($"Число {number1} не кратно числу {number2}. Остаток от деления {mod}");
-}
+int Number = digit1 * 10 + digit2;
 
+Console.WriteLine ($"Получаем число без второй цифры {Number}");
